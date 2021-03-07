8c69ff134627d95faeb7d62f2d03c137c39cb2cf
Mostafa Mohsen <mohsem1@mcmaster.ca>
2021-03-07 00:17:57 -0500

    added sample usage comment

--- predictor.py
+++ predictor.py
@@ -11,7 +11,10 @@ trained_model.compile(loss='mean_squared_error',
                 metrics=['accuracy'])
 
 def predict(features):
-
     input = np.array([features])
     out = trained_model.predict(input)
     return out.item()
+
+###### SAMPLE USAGE ########
+# sample_data = [0.2083, -1.0000, -1.0000, -0.8868, -0.4429, -1.0000, -1.0000, -0.2214, -1.0000, -0.6429,  0.0000, -1.0000,  0.3333]
+# predict(sample_data)
